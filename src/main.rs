3c4fd20c155d5ba4cb56f19e8a0d6b64461a37fa
logan gilmour <logangilmour@gmail.com>
2022-07-19 10:50:57 -0600

    simple tokenizer working with utf-8

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,82 @@
+use std::{
+    iter::Peekable,
+    str::{from_utf8_unchecked, CharIndices},
+};
+
 fn main() {
     println!("Hello, world!");
+}
+
+pub struct Tokenizer<'a> {
+    src_chars: Peekable<CharIndices<'a>>,
+    src_bytes: &'a [u8],
+}
+
+impl<'a> Tokenizer<'a> {
+    pub fn new(src: &'a str) -> Self {
+        Tokenizer {
+            src_chars: src.char_indices().peekable(),
+            src_bytes: src.as_bytes(),
+        }
+    }
+}
+
+pub fn is_brace(c: char) -> bool {
+    c == '(' || c == ')'
+}
+
+impl<'a> Iterator for Tokenizer<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut token_width;
+        let token_start;
+        loop {
+            if let Some((offset, first_char)) = self.src_chars.next() {
+                let width = first_char.len_utf8();
+                if first_char.is_ascii_whitespace() {
+                    continue;
+                }
+                if is_brace(first_char) {
+                    unsafe {
+                        return Some(from_utf8_unchecked(&self.src_bytes[offset..offset + width]));
+                    }
+                }
+                token_start = offset;
+                token_width = width;
+                break;
+            } else {
+                return None;
+            }
+        }
+        while let Some((_, next_char)) = self.src_chars.peek() {
+            let width = next_char.len_utf8();
+
+            if next_char.is_ascii_whitespace() || is_brace(*next_char) {
+                break;
+            }
+
+            token_width += width;
+            self.src_chars.next();
+        }
+        unsafe {
+            Some(from_utf8_unchecked(
+                &self.src_bytes[token_start..token_start + token_width],
+            ))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Tokenizer;
+
+    #[test]
+    fn test_tokenize() {
+        let test = "(☹️️)";
+        let parse = vec!["(", "☹️️", ")"];
+        assert!(Tokenizer::new(test)
+            .zip(parse.iter())
+            .all(|(a, b)| (&a).eq(b)));
+    }
 }
